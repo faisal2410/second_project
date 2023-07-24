5e99ad0d41cba6ab4ec769a818846494cf2c3fac
Faisal ahmed <faisal2410@yahoo.com>
2023-07-24 23:02:59 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Bangladesh")
+
+// npm init --y
+// npm install nodemon -D
+// "start-dev":"nodemon main.js"
+
+// npm run start-dev
+
+// .gitignore
+
+console.log("I love bangladesh")
